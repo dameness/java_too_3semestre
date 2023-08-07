aa4c74e5d0d8a47b737acf77822903f711dadebb
20222PF.CC0002 <20222PF.CC0002@pc-155963.passofundo.ifsul.local>
2023-08-07 11:40:51 -0300

    inicio do repo

---
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/java_too_3semestre/Java_too_3semestre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too.exercicios.respostas.java_too_3semestre;
+
+/**
+ *
+ * @author 20222PF.CC0002
+ */
+public class Java_too_3semestre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
